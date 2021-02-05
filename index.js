00e0763d90de97d4b6ab4d5aa49fcfc3ea8a1d85
Leo <leo.anderssonguillou@elev.ga.ntig.se>
2021-02-05 18:29:06 +0100

    Preview of ship on hover

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let grid2 = new Array(10).fill(0).map(_=>new Array(10).fill({}));
 let clickedCells = [];
 let boatsPlaced = 0;
 let gameState = 1;
+let col = "";
 
 const board1 = document.querySelector("#board1");
 const board2 = document.querySelector("#board2");
@@ -30,7 +31,10 @@ for (let y = 0; y < 10; y++) {
         click(x,y);
       });
       cell1.addEventListener("mouseenter", ()=> {
-        hover(x,y);
+        hover(x,y,true);
+      });
+      cell1.addEventListener("mouseleave", ()=> {
+        hover(x,y,false);
       });
 
       cell1.id = `${x}:${y}`;
@@ -77,21 +81,23 @@ function click(x, y){
   }
 }
 
-function hoverEnter(x, y){
+function hover(x, y, entering){
+  console.log("bababooey");
+  if (entering == true) {col = "lightgreen";}
+  else {col = "lightgray";}
   if (gameState == 1)
   {
     switch(boatsPlaced)
     {
-      case 0: placeShip(x,y);
+      case 0: previewShip(x,y,entering);
               break;
-      case 1: placeSub(x,y);
+      case 1: previewSub(x,y,entering);
               break;
-      case 2: placeShip(x,y);
+      case 2: previewShip(x,y,entering);
               break;
-      case 3: placeCarrier(x,y);
+      case 3: previewCarrier(x,y,entering);
               break;
-      case 4: placeSub(x,y);
-              gameState = 2;
+      case 4: previewSub(x,y,entering);
               break;
     }
   }
@@ -111,14 +117,11 @@ function placeShip(x, y){
     boatsPlaced++;
   }
 }
-function previewShip(x, y, entering){
-  col = "";
+function previewShip(x, y){
   if (clickedCells.includes(grid1[x][y]) || clickedCells.includes(grid1[x][y+1]) || clickedCells.includes(grid1[x][y-1]))
   {}
   else
   {
-    if (entering == true) {col = "lightgreen";}
-    else {col = "lightgray";}
     document.getElementById(grid1[x][y]).style.backgroundColor = col;
     document.getElementById(grid1[x][y+1]).style.backgroundColor = col;
     document.getElementById(grid1[x][y-1]).style.backgroundColor = col;
@@ -126,7 +129,7 @@ function previewShip(x, y, entering){
 }
 
 function placeSub(x, y){
-  if (clickedCells.includes(grid1[x][y]) || clickedCells.includes(grid1[x][y+1]))
+  if (clickedCells.includes(grid1[x][y]) || clickedCells.includes(grid1[x+1][y]))
   {}
   else
   {
@@ -135,6 +138,15 @@ function placeSub(x, y){
     document.getElementById(grid1[x+1][y]).style.backgroundColor = "lime";
     clickedCells.push(grid1[x+1][y]);
     boatsPlaced++;
+  }
+}
+function previewSub(x, y){
+  if (clickedCells.includes(grid1[x][y]) || clickedCells.includes(grid1[x+1][y]))
+  {}
+  else
+  {
+    document.getElementById(grid1[x][y]).style.backgroundColor = col;
+    document.getElementById(grid1[x+1][y]).style.backgroundColor = col;
   }
 }
 
@@ -152,6 +164,17 @@ function placeCarrier(x, y){
     document.getElementById(grid1[x-1][y]).style.backgroundColor = "lime";
     clickedCells.push(grid1[x-1][y]);
     boatsPlaced++;
+  }
+}
+function previewCarrier(x, y){
+  if (clickedCells.includes(grid1[x][y]) || clickedCells.includes(grid1[x+1][y]) || clickedCells.includes(grid1[x+2][y]) || clickedCells.includes(grid1[x-1][y]))
+  {}
+  else
+  {
+    document.getElementById(grid1[x][y]).style.backgroundColor = col;
+    document.getElementById(grid1[x+1][y]).style.backgroundColor = col;
+    document.getElementById(grid1[x+2][y]).style.backgroundColor = col;
+    document.getElementById(grid1[x-1][y]).style.backgroundColor = col;
   }
 }
 
